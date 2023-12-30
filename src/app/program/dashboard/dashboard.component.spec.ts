3a87c8a6c9956efcdb610744845496a744b370ce
Victor Cabello <vmeca87@gmail.com>
2023-12-30 15:40:11 -0600

    program/dashboard change element on click tab

--- src/app/program/dashboard/dashboard.component.spec.ts
+++ src/app/program/dashboard/dashboard.component.spec.ts
@@ -18,4 +18,50 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('showProgramList()', () => {
+    let programList:any;
+    let statistics:any;
+    beforeEach(() => {
+      programList = component.programList.nativeElement.classList;
+      statistics = component.statistics.nativeElement.classList;
+      spyOn(programList,'add');
+      spyOn(programList,'remove');
+      spyOn(statistics,'add');
+      spyOn(statistics,'remove');
+      // Execute
+      component.showProgramList();
+    });
+    it('should display progamList', () => {
+      expect(programList.add).toHaveBeenCalledWith('is-active');
+      expect(programList.remove).toHaveBeenCalledWith('is-hidden');
+    });
+    it('should hide statistics', () => {
+      expect(statistics.remove).toHaveBeenCalledWith('is-active');
+      expect(statistics.add).toHaveBeenCalledWith('is-hidden');
+    });
+  });
+
+  describe('showStatistics()', () => {
+    let programList:any;
+    let statistics:any;
+    beforeEach(() => {
+      programList = component.programList.nativeElement.classList;
+      statistics = component.statistics.nativeElement.classList;
+      spyOn(programList,'add');
+      spyOn(programList,'remove');
+      spyOn(statistics,'add');
+      spyOn(statistics,'remove');
+      // Execute
+      component.showStatistics();
+    });
+    it('should hide progamList', () => {
+      expect(programList.remove).toHaveBeenCalledWith('is-active');
+      expect(programList.add).toHaveBeenCalledWith('is-hidden');
+    });
+    it('should show statistics', () => {
+      expect(statistics.add).toHaveBeenCalledWith('is-active');
+      expect(statistics.remove).toHaveBeenCalledWith('is-hidden');
+    });
+  });
 });
--- src/app/program/dashboard/dashboard.component.ts
+++ src/app/program/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  @ViewChild('programList') programList!:ElementRef;
+  @ViewChild('statistics') statistics!:ElementRef;
+
+  showProgramList() : void {
+    this.programList.nativeElement.classList.add('is-active');
+    this.programList.nativeElement.classList.remove('is-hidden');
+    this.statistics.nativeElement.classList.add('is-hidden');
+    this.statistics.nativeElement.classList.remove('is-active');
+  }
+
+  showStatistics() : void {
+    this.statistics.nativeElement.classList.add('is-active');
+    this.statistics.nativeElement.classList.remove('is-hidden');
+    this.programList.nativeElement.classList.add('is-hidden');
+    this.programList.nativeElement.classList.remove('is-active');
+  }
 }
