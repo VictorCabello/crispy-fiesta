e4f73347faf4237b5183bf473aaefd65c93a9567
Victor Cabello <vmeca87@gmail.com>
2023-12-29 19:54:07 -0600

    add basic layout

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.sass']
+})
+export class NavbarComponent {
+
+  @ViewChild('navBurger') navBurger!: ElementRef;
+  @ViewChild('navMenu') navMenu!: ElementRef;
+
+
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
+  }
+}
